30dda304ade924aff2f23adc7727c5f22e0f916e
br0mb0 <brendan0805@gmail.com>
2025-03-09 05:16:55 -0400

    Added the toggle thing karamat wanted.

--- popup.js
+++ popup.js
@@ -2,13 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Popup loaded!");
 });
 
+function toggleImage() {
+    const img = document.getElementById("toggleImg");
+    img.src = img.src.includes("OFF.png") ? "images/ON.png" : "images/OFF.png";
+
+}
 document.addEventListener("DOMContentLoaded", function () {
     const page1 = document.getElementById("page1");
     const page2 = document.getElementById("page2");
     const page3 = document.getElementById("page3");
     const Webcam = document.getElementById("Webcam");
+    const toggleImg = document.getElementById("toggleImg");
     
-    
+
     if (page1) {  
         page1.addEventListener("click", () => {
             chrome.action.setPopup({ popup: "popup.html" });
@@ -33,14 +39,6 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
         console.error("Element #page3 not found");
     }
-    if (page3) {   
-        page3.addEventListener("click", () => {
-            chrome.action.setPopup({ popup: "popup3.html" });
-            window.location.href = "popup3.html";
-        });
-    } else {
-        console.error("Element #page3 not found");
-    }
     if (Webcam) {   
         Webcam.addEventListener("click", () => {
             chrome.action.setPopup({ popup: "Webcam.html" });
@@ -49,4 +47,9 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
         console.error("Element #Webcam not found");
     }
+    if (toggleImg)
+{    toggleImg.addEventListener("click", () => {
+        toggleImage();
+        });}
+
 });
